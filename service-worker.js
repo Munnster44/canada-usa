425c7babcfb0c8bbf3af61eb5c0a1980ff8d8d62
Munnster44 <89866372+Munnster44@users.noreply.github.com>
2026-02-07 21:07:33 -0500

    initial load

---
+++ service-worker.js
@@ -0,0 +1,30 @@
+const CACHE = "borderwatch-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./service-worker.js",
+  "./usa.geojson",
+  "./canada.geojson",
+  "./boundary.geojson",
+  "./icon-192.png",
+  "./icon-512.png"
+].filter(Boolean);
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).catch(() => {}));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(k => (k === CACHE ? null : caches.delete(k)))))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
+});
